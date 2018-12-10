4e0f1fc3a784151cea8688d91046369e811720b9
basantpatidar <basantpatidar33@gmail.com>
2018-12-10 16:19:40 -0500

    update

--- javascript/jsDiceGame/app.js
+++ javascript/jsDiceGame/app.js
@@ -6,10 +6,11 @@ roundScore = 0;
 //var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
-document.querySelector('score-0').textContent = '0';
-document.querySelector('score-1').textContent = '0';
-document.querySelector('current-0').textContent = '0';
-document.querySelector('current-1').textContent = '0';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
 
@@ -27,8 +28,31 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         if(dice !== 1){
             //Add roundScore
             roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
         }else{
             //Next Player
+            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+            roundScore =0;
 
+            document.getElementById('current-0').textContent ='0';
+            document.getElementById('current-1').textContent ='0';
+
+            document.querySelector('.player-0-panel').classList.toggle('active');
+            document.querySelector('.player-1-panel').classList.toggle('active');
+
+           // document.querySelector('.player-0-panel').classList.remove('active');
+           // document.querySelector('.player-1-panel').classList.add('active');
+
+           document.querySelector('.dice').style.display='none';
+            
         }
+});
+
+document.querySelector('btn-hold').addEventListener('click',function(){
+    // Add CURRENT score to GLOBAL score --
+
+    //Update the UI
+
+    //Check if the player won the Game
+
 });
