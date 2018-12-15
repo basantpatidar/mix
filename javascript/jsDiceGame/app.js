9fdbd4df658402879de4910e414f8dd6e74bf678
Basant Patidar <34193893+basantpatidar@users.noreply.github.com>
2018-12-14 22:57:37 -0500

    Update app.js

--- javascript/jsDiceGame/app.js
+++ javascript/jsDiceGame/app.js
@@ -24,6 +24,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         }
 });
 
+//get Hold buttton functional
 document.querySelector('.btn-hold').addEventListener('click',function(){
     if(gamePlaying){
         // Add CURRENT score to GLOBAL score --
